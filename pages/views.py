5b516af3f4472abbb0942713694436f2595b58f9
Savu Sebastian <savusebastianf@gmail.com>
2021-06-01 09:48:47 +0300

    a

--- pages/views.py
+++ pages/views.py
@@ -1677,6 +1677,318 @@ def results_schoolpointe_view(request):
 	return render(request, 'pages/export_page.html', context)
 
 
+def extra_links_view(request):
+	return render(request, 'pages/schoolpointe.html')
+
+
+def results_extra_links_view(request):
+	def clean_tags(tags):
+		for tag in tags:
+			tag.attrs.clear()
+
+			if tag.contents == [] or (len(tag.contents) < 2 and tag.contents[0] == '\xa0'):
+				tag.decompose()
+
+
+	def remove_tags(text):
+		div = re.compile(r'<div[^>]+>')
+		dive = re.compile(r'<div+>')
+		divc = re.compile(r'</div+>')
+		link = re.compile(r'<link[^>]+>')
+		section = re.compile(r'<section[^>]+>')
+		sectione = re.compile(r'<section+>')
+		sectionc = re.compile(r'</section+>')
+		article = re.compile(r'<article[^>]+>')
+		articlee = re.compile(r'<article+>')
+		articlec = re.compile(r'</article+>')
+		span = re.compile(r'<span+>')
+		spane = re.compile(r'<span[^>]+>')
+		spanc = re.compile(r'</span+>')
+		font = re.compile(r'<font+>')
+		fonte = re.compile(r'<font[^>]+>')
+		fontc = re.compile(r'</font+>')
+
+		text = div.sub('', text)
+		text = dive.sub('', text)
+		text = divc.sub('', text)
+		text = link.sub('', text)
+		text = section.sub('', text)
+		text = sectione.sub('', text)
+		text = sectionc.sub('', text)
+		text = article.sub('', text)
+		text = article.sub('', text)
+		text = articlec.sub('', text)
+		text = span.sub('', text)
+		text = spane.sub('', text)
+		text = spanc.sub('', text)
+		text = font.sub('', text)
+		text = fonte.sub('', text)
+		text = fontc.sub('', text)
+		text = re.sub('<!--|-->', '', text)
+
+		return text.strip()
+
+
+	def get_column(col):
+		col_images = col.find_all('img')
+		col_anchors = col.find_all('a')
+		col_tags = col.find_all(['article', 'b', 'button', 'col', 'colgroup', 'div', 'em', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'hr', 'ul', 'ol', 'li', 'p', 'table', 'td', 'th', 'tr', 'strong', 'input', 'label', 'legend', 'fieldset'])
+		clean_tags(col_tags)
+
+		while col.script != None:
+			col.script.decompose()
+
+		while col.style != None:
+			col.style.decompose()
+
+		while col.nav != None:
+			col.nav.decompose()
+
+		for image in col_images:
+			try:
+				if image.get('src') != None and image.get('src') != '':
+					src = image['src']
+
+					if 'alt' in image.attrs:
+						alt = image['alt']
+						image.attrs.clear()
+						image['alt'] = alt
+					else:
+						image.attrs.clear()
+						image['alt'] = 'alt-text'
+
+					image['src'] = src
+
+				else:
+					image.attrs.clear()
+
+				image['id'] = ''
+				image['role'] = 'presentation'
+				image['style'] = ''
+				image['width'] = '250'
+
+			except:
+				pass
+				# print('Image:', image)
+
+		for anchor in col_anchors:
+			try:
+				if anchor.get('href') != None and anchor.get('href') != '':
+					href = anchor['href']
+					anchor.attrs.clear()
+					anchor['href'] = href
+
+					if anchor.get('href')[:4] != 'http' and anchor.get('href').find('.pdf') == -1 and anchor.get('href').find('.txt') == -1\
+					and anchor.get('href').find('.xls') == -1 and anchor.get('href').find('.xlsx') == -1\
+					and anchor.get('href').find('.doc') == -1 and anchor.get('href').find('.docx') == -1\
+					and anchor.get('href').find('.ppt') == -1 and anchor.get('href').find('.pptx') == -1:
+						anchor.string = f'INTERNAL LINK {anchor.string}'
+
+			except:
+				pass
+				# print('Anchor:', anchor)
+
+		col = remove_tags(str(col))
+
+		return col
+
+
+	def get_content(web_page, post_main_content):
+		col1 = 'Flagged'
+		col2, col3, col4 = '', '', ''
+		col_num = '1'
+		page_nav = None
+		meta_title = ''
+		meta_keywords = ''
+		meta_desc = ''
+		form = ''
+		embed = ''
+		iframe = ''
+		calendar = ''
+		staff = ''
+		news = ''
+		issue_pages_counter = 0
+		# print(web_page)
+
+		# if web_page != '#':
+		try:
+			web_link = requests.get(web_page, timeout=5).content
+			web_soup = BeautifulSoup(web_link, 'html.parser')
+
+			if web_soup.find_all('meta', attrs={'name': 'title'}) != []:
+				meta_title = str(web_soup.find_all('meta', attrs={'name': 'title'}))
+
+			if web_soup.find_all('meta', attrs={'name': 'keywords'}) != []:
+				meta_keywords = str(web_soup.find_all('meta', attrs={'name': 'keywords'}))
+
+			if web_soup.find_all('meta', attrs={'name': 'description'}) != []:
+				meta_desc = str(web_soup.find_all('meta', attrs={'name': 'description'}))
+
+			if len(post_main_content.split('=')[0]) > 3:
+				if web_soup.find(class_=post_main_content.split('=')[1]).find_all('form') != []:
+					form = 'form'
+
+				if web_soup.find(class_=post_main_content.split('=')[1]).find_all('embed') != []:
+					embed = 'embed'
+
+				if web_soup.find(class_=post_main_content.split('=')[1]).find_all('iframe') != []:
+					iframe = 'iframe'
+
+				if web_soup.find(class_=post_main_content.split('=')[1]) != None and web_soup.find(class_=post_main_content.split('=')[1]) != '':
+					col1 = web_soup.find(class_=post_main_content.split('=')[1])
+					col1 = get_column(col1)
+				else:
+					issue_pages_counter = 1
+
+			else:
+				if web_soup.find(id=post_main_content.split('=')[1]).find_all('form') != []:
+					form = 'form'
+
+				if web_soup.find(id=post_main_content.split('=')[1]).find_all('embed') != []:
+					embed = 'embed'
+
+				if web_soup.find(id=post_main_content.split('=')[1]).find_all('iframe') != []:
+					iframe = 'iframe'
+
+				if web_soup.find(id=post_main_content.split('=')[1]) != None and web_soup.find(id=post_main_content.split('=')[1]) != '':
+					col1 = web_soup.find(id=post_main_content.split('=')[1])
+					col1 = get_column(col1)
+				else:
+					issue_pages_counter = 1
+
+			col1 = str(col1)
+
+			if len(col1) > 200000:
+				col1 = 'Flagged'
+				col2 = 'This page has too much content'
+				col3 = ''
+				col4 = ''
+				col_num = '2'
+				issue_pages_counter = 1
+			elif len(col1) > 150000:
+				col2 = col1[50000:100000]
+				col3 = col1[100000:150000]
+				col4 = col1[150000:]
+				col1 = col1[:50000]
+				col_num = '4'
+				issue_pages_counter = 1
+			elif len(col1) > 100000:
+				col2 = col1[50000:100000]
+				col3 = col1[100000:]
+				col1 = col1[:50000]
+				col_num = '3'
+			elif len(col1) > 50000:
+				col2 = col1[50000:]
+				col1 = col1[:50000]
+				col_num = '2'
+
+			if len(col4) > 50000:
+				col1 = 'Flagged'
+				col2 = 'This page has too much content'
+				col3 = ''
+				col4 = ''
+				col_num = '2'
+				issue_pages_counter = 1
+			elif len(col4) > 0:
+				col_num = '4'
+
+			return col1, col2, col3, col4, col_num, page_nav, meta_title, meta_keywords, meta_desc, form, embed, iframe, calendar, staff, news, issue_pages_counter
+
+		# else:
+		except:
+			issue_pages_counter = 1
+
+			return col1, col2, col3, col4, col_num, page_nav, meta_title, meta_keywords, meta_desc, form, embed, iframe, calendar, staff, news, issue_pages_counter
+
+
+	post_sites = request.POST.get('site')
+	post_school = request.POST.get('school_title')
+	post_main_content = request.POST.get('main_content')
+	print('>>', post_sites, post_school, post_main_content)
+
+	# Process inputs
+	all_sites = post_sites.split(',')
+
+	start_time = time()
+	mainfolder = all_sites[0].split('.')[1]
+	filepath = Path(f'static/files/{mainfolder}')
+	filepath.mkdir(parents=True, exist_ok=True)
+	files = {}
+
+	with open(f'../f_web_interface/static/files/{mainfolder}/report.csv', 'w', encoding='utf-8') as csv_report:
+		csv_report = csv.writer(csv_report)
+
+		page_counter = 0
+		issue_pages_counter = 0
+		split_slash = all_sites[0].split('/')
+		split_dot = all_sites[0].split('.')
+		split_mixed = all_sites[0].split('/')[2].split('.')
+		school_name = post_school
+
+		csv_report.writerow(['School name', school_name])
+
+		with open(f'../f_web_interface/static/files/{mainfolder}/{school_name}.csv', 'w', encoding='utf-8') as csv_main:
+			csv_writer = csv.writer(csv_main)
+			csv_writer.writerow(['Link to page', 'Tier 1', 'Tier 2', 'Tier 3', 'Tier 4', 'Tier 5', 'Column Count', 'Column 1', 'Column 2', 'Column 3', 'Column 4', 'Meta title', 'Meta keywords', 'Meta description'])
+
+			for link in all_sites:
+				tiers = link.split('/')
+				t1, t2, t3, t4, t5 = '', '', '', '', ''
+				if len(tiers) == 4:
+					t1 = tiers[-1].capitalize()
+				elif len(tiers) == 5:
+					t1 = tiers[-2].capitalize()
+					t2 = tiers[-1].capitalize()
+				elif len(tiers) == 6:
+					t1 = tiers[-3].capitalize()
+					t2 = tiers[-2].capitalize()
+					t3 = tiers[-1].capitalize()
+				elif len(tiers) == 7:
+					t1 = tiers[-4].capitalize()
+					t2 = tiers[-3].capitalize()
+					t3 = tiers[-2].capitalize()
+					t4 = tiers[-1].capitalize()
+				elif len(tiers) == 8:
+					t1 = tiers[-5].capitalize()
+					t2 = tiers[-4].capitalize()
+					t3 = tiers[-3].capitalize()
+					t4 = tiers[-2].capitalize()
+					t5 = tiers[-1].capitalize()
+				else:
+					print(len(tiers))
+
+				page_link = link
+
+				page_counter += 1
+				col1, col2, col3, col4, col_num, nav_sec, meta_title, meta_keywords, meta_desc, form, embed, iframe, calendar, staff, news, content_ipc = get_content(page_link, post_main_content)
+				issue_pages_counter += content_ipc
+
+				csv_writer.writerow([str(page_link), t1, t2, t3, t4, t5, col_num, col1, col2, col3, col4, meta_title, meta_keywords, meta_desc])
+
+				if form != '' or embed != '' or iframe != '' or calendar != '' or staff != '' or news != '':
+					csv_report.writerow([str(page_link), form, embed, iframe, calendar, staff, news])
+
+			csv_report.writerow([])
+			csv_report.writerow(['Pages scraped', page_counter])
+			csv_report.writerow(['Issues', issue_pages_counter])
+			csv_report.writerow([])
+			csv_report.writerow([])
+			csv_report.writerow([])
+
+			files[f'{school_name}.csv'] = f'static/files/{mainfolder}/{school_name}.csv'
+			print('Finished:', site)
+
+	files['report.csv'] = f'static/files/{mainfolder}/report.csv'
+	print('Finished:', round((time() - start_time) / 60, 2), 'minutes')
+
+	context = {
+		'files': files,
+		'time': f'{str(round((time() - start_time) / 60, 2))} minutes'
+	}
+
+	return render(request, 'pages/export_page.html', context)
+
+
 def vpn_view(request):
 	return render(request, 'pages/vpn.html')
 
