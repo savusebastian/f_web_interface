946140d4827a6fd515775870d17c187cb91ebf89
Savu Sebastian <savusebastianf@gmail.com>
2022-06-03 22:25:22 +0300

    Updates

--- static/js/main.js
+++ static/js/main.js
@@ -1,4 +1,5 @@
 function main() {
+	const main = document.querySelector('main');
 	const form = document.querySelector('form');
 	const side_nav = document.querySelector('.side-nav');
 	const main_content = document.querySelector('.main-content');
@@ -6,13 +7,14 @@ function main() {
 	const loading_element = document.querySelector('.loading-element');
 
 	if(form) {
-		form.addEventListener('submit', function(){
-			side_nav.style.display = 'none';
-			main_content.style.display = 'none';
-			loading_element.style.display = 'block';
+		form.addEventListener('submit', function() {
+			main.classList.add('center');
+			side_nav.classList.add('hidden');
+			main_content.classList.add('hidden');
+			loading_element.classList.remove('hidden');
 
 			if(side_content) {
-				side_content.style.display = 'none';
+				side_content.classList.add('hidden');
 			}
 		});
 	}
